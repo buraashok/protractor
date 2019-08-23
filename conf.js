4b14270eee80507497116f95d1c2ccefd1d6ce2e
buraashok <46172238+buraashok@users.noreply.github.com>
2019-08-23 15:13:06 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,4 +1,4 @@
-var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+//var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 exports.config = {
 //framework: 'jasmine',
  //framework: 'jasmine2',
@@ -20,13 +20,13 @@ specs: ['spec.js'],
           'moz:firefoxOptions': {
                args: [ "--headless" ]
                   }
-                   },
+                   }
                     
-  onPrepare: function() {
-      jasmine.getEnv().addReporter(
-        new Jasmine2HtmlReporter({
-          savePath: 'target/screenshots'
-        })
-      );
-   }
+  //onPrepare: function() {
+    //  jasmine.getEnv().addReporter(
+      //  new Jasmine2HtmlReporter({
+        //  savePath: 'target/screenshots'
+        //})
+      //);
+  // }
 };
