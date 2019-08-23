7fe7355c5bf97520f3c786d53c5dac2bd9a8c069
buraashok <46172238+buraashok@users.noreply.github.com>
2019-08-23 14:59:55 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,27 +1,25 @@
 exports.config = {
-framework: 'jasmine',
+//framework: 'jasmine',
+ //framework: 'jasmine2',
+ //onPrepare: function() {
+   //var AllureReporter = require('jasmine-allure-reporter');
+   //jasmine.getEnv().addReporter(new AllureReporter({
+     //resultsDir: 'allure-results'
 seleniumAddress: 'http://localhost:4444/wd/hub',
 specs: ['spec.js'],
+//capabilities: {
+//   //browserName: 'chrome',
+//      //chromeOptions: { args: ["--disable-dev-shm-usage", "--headless", "--no-sandbox"] },
+//      //binary: "/tmp/chromedriver -port 9515"
+//        //   }
+//        //}
+//
        capabilities: {
          browserName: 'firefox',
           'moz:firefoxOptions': {
                args: [ "--headless" ]
-    }
-  },
- // onPrepare: function() {
-   //   jasmine.getEnv().addReporter(
-     //   new Jasmine2HtmlReporter({
-       //   savePath: 'target/screenshots'
-       
-       onPrepare: function(){
-       var AllureReporter = require('workspace/allure results');
-      jasmine.getEnv().addReporter(new AllureReporter({
-  resultDir: 'target/allure-results'
-             
-        })
-      );
-   }
-};
-   
+                  }
+                   }
+                   };
 
 
