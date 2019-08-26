c7ed168e8ae1379e2f00d5e9ba57579868e09303
buraashok <46172238+buraashok@users.noreply.github.com>
2019-08-26 15:31:43 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -10,17 +10,26 @@
               //     };
 
 
-
-
-framework: 'jasmine2',
 onPrepare: function() {
-    var jasmineReporters = require('jasmine-reporters');
-    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
-        consolidateAll: true,
-        savePath: 'testresults',
-        filePrefix: 'xmloutput'
-    })),
-}
+  // The require statement must be down here, since jasmine-reporters
+  // needs jasmine to be in the global and protractor does not guarantee
+  // this until inside the onPrepare function.
+  require('jasmine-reporters');
+  jasmine.getEnv().addReporter(
+    new jasmine.JUnitXmlReporter('xmloutput', true, true));
+},
+
+
+
+//framework: 'jasmine2',
+//onPrepare: function() {
+  //  var jasmineReporters = require('jasmine-reporters');
+    //jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+      //  consolidateAll: true,
+        //savePath: 'testresults',
+        //filePrefix: 'xmloutput'
+    //})),
+//}
 exports.config = { 
  seleniumAddress: 'http://18.212.163.27:4444/wd/hub',
 specs: ['spec.js'],
