6876e73ee3fa3304bed4c4a6c7302db0b703fc27
buraashok <46172238+buraashok@users.noreply.github.com>
2019-08-26 14:55:47 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,4 +1,28 @@
+//exports.config = {
+ //seleniumAddress: 'http://18.212.163.27:4444/wd/hub',
+//specs: ['spec.js'],
+  //     capabilities: {
+    //     browserName: 'firefox',
+      //    'moz:firefoxOptions': {
+        //       args: [ "--headless" ]
+          //        }
+            //       }
+              //     };
+
 exports.config = {
+
+jasmine 
+ 
+framework: 'jasmine2',
+onPrepare: function() {
+    var jasmineReporters = require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+        consolidateAll: true,
+        savePath: 'testresults',
+        filePrefix: 'xmloutput'
+    })),
+}
+ 
  seleniumAddress: 'http://18.212.163.27:4444/wd/hub',
 specs: ['spec.js'],
        capabilities: {
@@ -7,7 +31,7 @@ specs: ['spec.js'],
                args: [ "--headless" ]
                   }
                    }
-                   },
+                   };
 
 //framework: 'jasmine2',
 onPrepare: function() {
