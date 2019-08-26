ddf675358c296f67ce462fa8bfe4adfbc9b5832a
buraashok <46172238+buraashok@users.noreply.github.com>
2019-08-26 15:43:37 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,3 +1,19 @@
+//exports.config = {
+//seleniumAddress: 'http://18.212.163.27:4444/wd/hub',
+//specs: ['spec.js'],
+  //   capabilities: {
+    // browserName: 'firefox',
+    //'moz:firefoxOptions': {
+      // args: [ "--headless" ]
+        //}
+       //}
+     //};
+
+
+
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+  
+
 exports.config = {
 seleniumAddress: 'http://18.212.163.27:4444/wd/hub',
 specs: ['spec.js'],
@@ -6,7 +22,15 @@ specs: ['spec.js'],
     'moz:firefoxOptions': {
        args: [ "--headless" ]
         }
-       }
-     };
+       },
+
+  onPrepare: function() {
+      jasmine.getEnv().addReporter(
+        new Jasmine2HtmlReporter({
+          savePath: 'target/screenshots'
+        })
+      );
+   }
+};
 
 
